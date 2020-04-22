9ad4f9a8c61ce6c69d57d57fa3687bb6c049eff3
Param-Bhatt <f20180862@hyderabad.bits-pilani.ac.in>
2020-04-22 19:59:43 +0530

    adding points work with a left click

--- Main.cpp
+++ Main.cpp
@@ -4,138 +4,243 @@
 #include <math.h>
 #include <vector>
 using namespace std;
+
+// simplemouse.cpp
+// by Glenn G. Chappell
+// September 2003
+//
+// For CS 381
+// Simple Mouse Demo
+//
+// Draws a red box at the mouse position
+// whenever the left mouse button is down.
+
+
+// Global variables
+// Window/viewport
+const int startwinsize = 400; // Starting window width & height, in pixels
+int winw, winh;               // Window width & height, in pixels, saved by reshape
+
+// Mouse
+bool mouseleftdown = false;   // True if mouse LEFT button is down.
+                              // Saved by mouse.
+int mousex, mousey;           // Mouse x,y coords, in GLUT format (pixels from upper-left corner).
+                              // Only guaranteed to be valid if a mouse button is down.
+                              // Saved by mouse, motion.
+
+// Keyboard
+const int ESCKEY = 27;        // ASCII value of escape character
+
+// Other
+const double pointsize = 40;  // Size of point
 struct vertex
 {
-	int x, y;
+    double x, y;
 };
 vector<vertex> v;
-vertex merge(vertex v1, vertex v2, float t) 
+vertex merge(vertex v1, vertex v2, double t)
 {
-	vertex out;
-	out.x = t * v1.x + (1 - t) * v2.x;
-	out.y = t * v1.y + (1 - t) * v2.y;
-	return out;
+    vertex out;
+    out.x = t * v1.x + (1 - t) * v2.x;
+    out.y = t * v1.y + (1 - t) * v2.y;
+    return out;
 }
-vector<vertex> findOneLess(vector<vertex> vert, float t)
+vector<vertex> findOneLess(vector<vertex> vert, double t)
 {
-	vector<vertex> f;
-	for (int i = 0; i < vert.size() - 1; i++)
-	{
-		f.push_back(merge(vert[i], vert[i + 1], t));
-	}
-	return f;
+    if (vert.size() != 1)
+    {
+        vector<vertex> f;
+        for (int i = 0; i < vert.size() - 1; i++)
+        {
+            f.push_back(merge(vert[i], vert[i + 1], t));
+        }
+        return f;
+    }
 }
-vertex findFinalVert(float t)
+vertex findFinalVert(double t)
 {
-	vector<vertex> vtr;
-	vtr = findOneLess(v, t);
-	while (vtr.size() != 1)
-	{
-		vtr = findOneLess(vtr, t);
-	}
-	return vtr[0];
-}
-void renderScene(void) {
-	//Clear all pixels
+    if (v.size() < 2)
+    {
+        vertex vecto{};
+        vecto.x = NULL;
+        vecto.y = NULL;
+        return vecto;
+    }
 
-	glLoadIdentity();
-	glOrtho(0.0, 1000, 0.0, 1000, -1.0, 1.0);
-	//glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(1.0, 1.0, 1.0);
-	glPointSize(2.0);
-	glBegin(GL_POINTS);
-	vertex p1, p2, p3, p4;
-	//Point 1
-	p1.x = 100;
-	p1.y = 200;
-	v.push_back(p1);
-	//Point 2
-	p2.x = 300;
-	p2.y = 800;
-	v.push_back(p2);
-	//Point 3
-	p3.x = 500;
-	p3.y = 200;
-	v.push_back(p3);
-	//Point 4
-	p4.x = 700;
-	p4.y = 800;
-	v.push_back(p4);
-	/*glVertex2i(p1.x, p1.y);
-	glVertex2i(p2.x, p2.y);/*
-	glVertex2i(p3.x, p3.y);
-	glVertex2i(p4.x, p4.y);*/
-	for (float i = 0; i < 1; i+=0.001)
-	{
-		vertex finalvert;
-		finalvert = findFinalVert(i);
-		glVertex2i(finalvert.x, finalvert.y);
-	}
-	glEnd();
-
-	glutSwapBuffers();
-}
-void changeSize(int w, int h) {
-
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
-	if (h == 0)
-		h = 1;
-
-	float ratio = w * 1.0 / h;
-
-	// Use the Projection Matrix
-	glMatrixMode(GL_PROJECTION);
-
-	// Reset Matrix
-	glLoadIdentity();
-
-	// Set the viewport to be the entire window
-	glViewport(0, 0, w, h);
-
-	// Set the correct perspective.
-	gluPerspective(45, ratio, 1, 100);
-
-	// Get Back to the Modelview
-	glMatrixMode(GL_MODELVIEW);
-}
-void init() {
-	//select clearing (background) color
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-
-	//initialize viewing values 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 1000, 0.0, 1000, -1.0, 1.0);
+    vector<vertex> vtr;
+    vtr = findOneLess(v, t);
+    while (vtr.size() != 1)
+    {
+        vtr = findOneLess(vtr, t);
+    }
+    return vtr[0];
 }
 
-int main(int argc, char** argv) {
-
-	//Initialise GLUT with command-line parameters. 
-	glutInit(&argc, argv);
-	
-	//Set Display Mode
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
-	//Set the window size
-	glutInitWindowSize(750, 750);
-
-	//Set the window position
-	glutInitWindowPosition(100, 100);
-
-	//Create the window
-	glutCreateWindow("Biezer Curve");
-
-	//Call init (initialise GLUT
-	init();
-
-	//Call "display" function
-	glutDisplayFunc(renderScene);
-
-	//Enter the GLUT event loop
-	glutMainLoop();
-
-	return 0;
+// printbitmap
+// Prints the given string at the given raster position
+//  using GLUT bitmap fonts.
+// You probably don't want any rotations in the model/view
+//  transformation when calling this function.
+void printbitmap(const string msg, double x, double y)
+{
+    glRasterPos2d(x, y);
+    for (string::const_iterator ii = msg.begin();
+        ii != msg.end();
+        ++ii)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *ii);
+    }
 }
 
+
+// display
+// The GLUT display function
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Make a small box at the mouse position, if the LEFT button is down
+    // Draw the box
+    glColor3d(0.0, 0.6, 0.6);
+    glPointSize(2.0);
+    glBegin(GL_POINTS);
+    for (int i = 0; i < v.size(); i++)
+    {
+        glVertex2f(v[i].x, v[i].y);
+    }
+    for (double i = 0; i < 1; i += 0.001)
+    {
+        vertex final;
+        final = findFinalVert(i);
+        glVertex2d(final.x, final.y);
+    }
+    glEnd();
+    // Documentation
+    glColor3d(1.0, 1.0, 1.0);  // White text
+    printbitmap("Bezier Curve", 0.05, 0.95);
+    printbitmap("Press Left mouse button to add point", 0.05, 0.9);
+    printbitmap("Press Middle Button to drag the nearest point", 0.05, 0.85);
+    printbitmap("Press Right Button to Remove the nearest point", 0.05, 0.80);
+    glutSwapBuffers();
+}
+
+
+// mouse
+// The GLUT mouse function
+void mouse(int button, int state, int x, int y)
+{
+    // Save the left button state
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        mouseleftdown = (state == GLUT_DOWN);
+        mousex = x;
+        mousey = y;
+        double oglx = double(mousex) / winw;
+        double ogly = 1 - double(mousey) / winh;
+        cout << "X: " << oglx;
+        cout << "Y: " << ogly << endl;
+        vertex vert;
+        vert.x = oglx;
+        vert.y = ogly;
+        v.push_back(vert);
+        //glutPostRedisplay();  // Left button has changed; redisplay!
+    }
+
+    // Save the mouse position
+    
+}
+
+
+// motion
+// The GLUT motion function
+void motion(int x, int y)
+{
+    // We only do anything if the left button is down
+    if (mouseleftdown)
+        //glutPostRedisplay();
+
+    // Save the mouse position
+    mousex = x;
+    mousey = y;
+}
+
+
+// idle
+// The GLUT idle function
+void idle()
+{
+    // Print OpenGL errors, if there are any (for debugging)
+    if (GLenum err = glGetError())
+    {
+        cerr << "OpenGL ERROR: " << gluErrorString(err) << endl;
+    }
+}
+
+
+// keyboard
+// The GLUT keyboard function
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case ESCKEY:  // ESC: Quit
+        exit(0);
+        break;
+    }
+}
+
+
+// reshape
+// The GLUT reshape function
+void reshape(int w, int h)
+{
+    // Set the viewport to be the entire drawing area of the window
+    glViewport(0, 0, w, h);
+
+    // Save the window size
+    winw = w;
+    winh = h;
+
+    // Set up the projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 1.0, 0.0, 1.0);
+
+    glMatrixMode(GL_MODELVIEW);  // Always go back to model/view mode
+}
+
+
+// init
+// Initializes GL states
+// Called by main after window creation
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+
+int main(int argc, char** argv)
+{
+    // Initialize OpenGL/GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+    // Make a window
+    glutInitWindowSize(startwinsize, startwinsize);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Bezier Curves");
+   
+    // Initialize GL states & register callbacks
+    init();
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+
+    // Do something
+    glutMainLoop();
+
+    return 0;
+}
